226372d5f4d530728133099145345a85303add25
anonymousearly <elaco.earl13@gmail.com>
2023-05-06 23:04:07 +0800

    our output

---
+++ app.js
@@ -0,0 +1,8 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const result = document.querySelector(".result");
+
+const matchresult = {
+
+}
